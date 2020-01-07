57ac3ce200918d2b93ac7f2a6dd239139a95bcdd
watche520 <55218617+watche520@users.noreply.github.com>
2020-01-07 22:40:41 +0800

    VR

---
+++ Unity_VR_B1040406_MaJaJa/Assets/button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button : MonoBehaviour
+{
+    public void REEE()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void EXXX() {
+        Application.Quit();
+    }
+
+}
---
+++ Unity_VR_B1040406_MaJaJa/Assets/suriken.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class suriken : MonoBehaviour
+{
+    public GameObject Cam1, Cam2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void addF()
+    {
+        if (Cam1.activeInHierarchy == true)
+        {
+            this.GetComponent<Rigidbody>().AddForce(Cam1.gameObject.transform.forward * 200);
+        }
+        else
+        {
+            this.GetComponent<Rigidbody>().AddForce(Cam2.gameObject.transform.forward * 200);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "GGG")
+        {
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
